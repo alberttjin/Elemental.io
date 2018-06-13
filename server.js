08f5e3b8e4ce4dd6b3a74ce0d826ccc5e7eff059
alberttjin <albertjin@berkeley.edu>
2018-06-13 01:43:54 -0700

    changed to server index.html, use that to test, will work on seperate branch now

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/client/index.html');
+});
+
 
 const PORT = process.env.PORT
 app.listen(PORT || 8081)
