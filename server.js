09b8f83782e1a9c76e7aeac7b22dbb3e93e4d2c9
Alden Liu <aldenliu98@berkeley.edu>
2018-06-25 16:21:37 -0700

    hey babe

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
 
-//app.get('/', (req, res) => res.send('Hello World!'))
-
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/client/index.html');
 });
 
-
-const PORT = process.env.PORT
-app.listen(PORT || 8081)
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`);
+});
