ea39c9b6a71e505b4d98e3c90a5d550772a374d7
Alden Liu <Alden.Liu@ibm.com>
2018-07-06 12:33:38 -0500

    sockets with rooms

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-//app.get('/', (req, res) => res.send('Hello World!'))
+const PORT = process.env.PORT || 3000;
+
+const namespace = '/';
+const roomPrefix = 'room-';
+const maxPlayers = 4;
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/client/index.html');
 });
 app.use(express.static(__dirname + "/Client"));
-const PORT = process.env.PORT
-app.listen(PORT || 2000)
+
+var socketToRoomNum = new Map();
+var stack = [1];
+io.on('connection', (socket) => {
+	var nextAvailiableRoom = stack.pop();
+	if (io.nsps[namespace].adapter.rooms[roomPrefix + nextAvailiableRoom] && 
+		io.nsps[namespace].adapter.rooms[roomPrefix + nextAvailiableRoom].length == maxPlayers) {
+			nextAvailiableRoom ++;
+		}
+	socket.join(roomPrefix + nextAvailiableRoom);
+	socketToRoomNum.set(socket.id, nextAvailiableRoom);
+	stack.push(nextAvailiableRoom);
+	console.log(`Socket (${socket.id}) has joined room ${nextAvailiableRoom}`);
+
+  	socket.on('disconnect', () => {
+		var socketRoom = socketToRoomNum.get(socket.id);
+		console.log(`Socket (${socket.id}) has left room ${nextAvailiableRoom}`);
+		socketToRoomNum.delete(socket.id);
+		stack.push(socketRoom);
+  });
+});
+
+http.listen(PORT, function() {
+	console.log(`Listening on port: ${PORT}`);
+});
